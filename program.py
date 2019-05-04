549099e0a1548fa6a00b843a2fa0c8c318fd6919
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-05-04 19:39:42 +0100

    application entry point open file

---
+++ program.py
@@ -0,0 +1,8 @@
+def main():
+    # TODO (Stephen): grab data from quandl rather than loading from file
+    dataDirectory = "./data/"
+    f = open(dataDirectory + "trainingsample" + ".csv", "r")
+
+
+if __name__ == "__main__":
+    main()
